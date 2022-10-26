d6e01457a5b7091a88ded677f6662774344894f8
ROBIN <103017721+ItsMeAlcazar@users.noreply.github.com>
2022-10-27 04:06:42 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,27 +1,29 @@
 echo 'Starting to clone stuffs needed for your device'
 
 echo 'Common Device Tree [1/6]'
-# common-dt
-git clone https://github.com/Iris0411/device_xiaomi_msm8953-common -b 13 device/xiaomi/msm8953-common
+# Common-device Tree
+git clone https://github.com/FusionOS-Devices/android_device_xiaomi_msm8953-common.git -b 12 device/xiaomi/msm8953-common
 
-echo 'Cloning Prebuilt Kernel tree [2/6]'
+echo 'Cloning Vendor tree [2/6]'
 # Vendor Common Tree
-git clone https://github.com/Iris0411/vendor_xiaomi_ysl vendor/xiaomi/ysl
+git clone https://github.com/FusionOS-Devices/android_vendor_xiaomi_ysl -b 12 vendor/xiaomi/ysl
 
-echo 'Cloning Vendor tree [3/6]'
+echo 'Cloning Vendor Common Tree [3/6]'
 # Vendor Tree
-git clone https://github.com/Iris0411/vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
+git clone https://github.com/FusionOS-Devices/android_vendor_xiaomi_msm8953-common -b 12 vendor/xiaomi/msm8953-common
 
 echo 'Cloning Kernel tree [4/6]'
 # Kernel Tree
-git clone --depth=1 https://github.com/DhruvChhura/kernel_xiaomi_ysl kernel/xiaomi/ysl
+git clone https://github.com/sushmit1/kernel_xiaomi_ysl.git -b 12 kernel/xiaomi/msm8953 kernel/xiaomi/msm8953
 
 echo 'Cloning Proton Clang [5/6]'
 # Proton Clang
-git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton &&
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/darwin-x86/clang-proton
 
 echo 'Cloning Hardware Xiaomi' [6/6]
 # Hardware Xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+git clone https://github.com/sushmit1/hardware_qcom-caf_ysl -b display-msm8996-s hardware/qcom-caf/msm8996/display && git clone https://github.com/sushmit1/hardware_qcom-caf_ysl -b media-msm8996-s hardware/qcom-caf/msm8996/media && git clone https://github.com/sushmit1/hardware_qcom-caf_ysl -b audio-msm8996-s hardware/qcom-caf/msm8996/audio
+
 
 echo 'Completed, proceeding to lunch'
